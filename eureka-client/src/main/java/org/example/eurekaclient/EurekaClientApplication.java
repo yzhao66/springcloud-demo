08b612b771bacf94adea5b2a0126b7e09cb6c32b
yzhao66 <123456yzh>
2020-05-20 23:02:02 +0800

    提交

--- eureka-client/src/main/java/org/example/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/org/example/eurekaclient/EurekaClientApplication.java
@@ -3,13 +3,13 @@ package org.example.eurekaclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
-@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
