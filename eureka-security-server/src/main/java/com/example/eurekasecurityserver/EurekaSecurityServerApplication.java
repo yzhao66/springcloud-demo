b36e597be0b30a403e8edf579890c46bb59a2bbd
yangzhuohao <yangzhuohao@cecii.cn>
2021-03-24 16:41:12 +0800

    提交

---
+++ eureka-security-server/src/main/java/com/example/eurekasecurityserver/EurekaSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekasecurityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSecurityServerApplication.class, args);
+	}
+
+}
